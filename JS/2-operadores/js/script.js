ffe4a054f1800a05a97489a1301ab1b1a59857c9
joelsondeveloper <115474776+joelsondeveloper@users.noreply.github.com>
2024-02-05 19:36:25 -0300

    operadores

---
+++ JS/2-operadores/js/script.js
@@ -0,0 +1,37 @@
+/* // Number
+// console.log(typeof 2)
+// console.log(typeof 5.14)
+// console.log(typeof -127)
+
+// // aritmetica
+// console.log(2 + 4)
+// console.log(10 - 5)
+// console.log(5 * 4)
+// console.log(10 / 2)
+// console.log(5 + (4 * 2))
+
+// // special numbers
+// console.log(typeof Infinity)
+// console.log(typeof -Infinity)
+// console.log(12 * "asd")
+// console.log(typeof NAN)
+
+// // strings
+// console.log("um texto")
+// console.log('mais um texto')
+// console.log(`outro texto`)
+// console.log(typeof "7")
+// console.log(typeof `78`)
+*/
+
+// simbolos especiais em string
+console.log("testando a \nquebra de linha")
+console.log("espaçamento \t de tab")
+
+// concatenação
+console.log("oi, " + "tudo " + "bem")
+console.log(`Testando ` + `com ` + `crase`)
+
+// interpolação
+console.log(`a soma de 2 + 2 é= ${2 + 2}`)
+console.log(`podemos executar qualquer coisa aqui ${console.log("teste")}`)
