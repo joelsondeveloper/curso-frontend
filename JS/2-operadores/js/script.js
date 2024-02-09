ccf1ebc9838e3d9a853665ccaa32437cfd3a9dfb
joelsondeveloper <115474776+joelsondeveloper@users.noreply.github.com>
2024-02-09 15:57:54 -0300

    aprendi operadores

--- JS/2-operadores/js/script.js
+++ JS/2-operadores/js/script.js
@@ -22,7 +22,7 @@
 // console.log(`outro texto`)
 // console.log(typeof "7")
 // console.log(typeof `78`)
-*/
+
 
 // simbolos especiais em string
 console.log("testando a \nquebra de linha")
@@ -35,3 +35,45 @@ console.log(`Testando ` + `com ` + `crase`)
 // interpolação
 console.log(`a soma de 2 + 2 é= ${2 + 2}`)
 console.log(`podemos executar qualquer coisa aqui ${console.log("teste")}`)
+
+// boolean
+console.log(true)
+console.log(5 > 20)
+console.log(30 > 10)
+console.log(typeof false)
+
+// comparações
+console.log(5 <= 5)
+console.log(5 < 5)
+console.log(10 == 10)
+console.log(10 == 9)
+console.log(10 != 9)
+
+// identico
+console.log(9 == "9")
+console.log(9 === "9")
+console.log(9 != "9")
+console.log(9 !== "9")
+*/
+// operadores logicos
+console.log(true && true)
+console.log(true && false)
+console.log(5 > 2 && 2 < 10)
+console.log(5 > 2 && "matheus" === 1)
+console.log(5 > 2 || "matheus" === 1)
+console.log(5 < 2 || 5 > 100)
+console.log(!true)
+console.log(!5 > 2)
+
+// empty values
+console.log(typeof null, typeof undefined)
+console.log(null == undefined)
+console.log(null === undefined)
+console.log(undefined == false)
+
+// conversão automatica
+console.log(5 * null)
+console.log("teste" * "opa")
+console.log("10" + 1)
+console.log("10" - 1)
+
