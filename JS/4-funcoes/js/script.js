9c07418a3fb2948a38bd7d0d8ba8cb993de15add
joelsondeveloper <115474776+joelsondeveloper@users.noreply.github.com>
2024-02-19 05:38:19 -0300

    terminei funções

--- JS/4-funcoes/js/script.js
+++ JS/4-funcoes/js/script.js
@@ -152,3 +152,61 @@ const repeatText = (text, repeat = 2) => {
 
 console.log(repeatText("olá"))
 console.log(repeatText("olá2", 5))
+
+// closure
+
+function someFunction() {
+    let txt ="Faça alguma coisa"
+
+    function display() {
+        console.log(txt)
+    }
+    display()
+}
+
+someFunction()
+
+// mais sobre closure
+const multiplicationClosure = (n) => {
+    return (m) => {
+        return n * m
+    }
+}
+
+const c1 = multiplicationClosure(5)
+const c2 = multiplicationClosure(10)
+
+console.log(c1)
+console.log(c2)
+
+console.log(c1(5))
+console.log(c2(10))
+
+// recursão
+const untilTen = (n, m) => {
+    if(n < 10) {
+        console.log("A função parou de executar")
+    } else {
+        const x = n - m
+
+        console.log(x)
+
+        untilTen(x, m)
+    }
+}
+
+untilTen(100, 7)
+
+function factorial(x) {
+    if(x === 0) {
+        return 1
+    } else {
+        return x * factorial(x - 1)
+    }
+}
+
+const num = 6
+
+const result = factorial(num)
+
+console.log(`O fatorial do numero ${num} é ${result}`)
---
+++ JS/5-arrays_e_objetos/js/scripts.js
@@ -0,0 +1,57 @@
+// arrays
+const lista = [1, 2, 3, 4, 5]
+
+console.log(lista)
+
+console.log(typeof lista)
+
+const itens = ["Matheus", true, 2, 4.12, []]
+
+console.log(itens)
+// mais sobre arrays
+const arr = ["a", "b", "c", "d",]
+
+console.log(arr[0])
+console.log(arr[2])
+console.log(arr[95])
+
+// propiedades
+const numbers = [5, 3, 4]
+
+console.log(numbers.length)
+
+console.log(numbers["length"])
+
+const myName = "Matheus"
+
+console.log(myName.length)
+
+// metodos
+const otherNumbers = [1, 2, 3]
+
+const allNumbers = numbers.concat(otherNumbers)
+
+console.log(allNumbers)
+
+const text = "algum texto"
+
+console.log(text.toUpperCase())
+
+console.log(typeof text.toUpperCase)
+
+console.log(text.indexOf("g"))
+
+// objetos
+const person = {
+    name: "Joelson",
+    age: 18,
+    job: "progamador"
+}
+
+console.log(person)
+
+console.log(person.name)
+
+console.log(person.name.length)
+
+console.log(typeof person)
